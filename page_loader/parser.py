1f15821e96164b1bb513457ba40727088c5f34b3
Ivan Aleksashin <algos.inc@gmail.com>
2022-04-10 01:51:11 +0200

    fix logic and add documentation

--- page_loader/parser.py
+++ page_loader/parser.py
@@ -1,9 +1,12 @@
+"""Module for parsing the page and extracting the data."""
 import os
+import logging
+
 from secrets import token_urlsafe
 from bs4 import BeautifulSoup
 from typing import List
 from urllib.parse import urlparse, urljoin
-from page_loader.scripts.definitions import ALLOWED_TAGS
+
 '''
 + get file_path, resources_path
 + open html file and read
@@ -14,7 +17,7 @@ from page_loader.scripts.definitions import ALLOWED_TAGS
 - return queue
 '''
 
-
+# allowed resources for downloading
 RESOURCES = {  # noqa: WPS407
     'img': 'src',
     'link': 'href',
@@ -22,54 +25,77 @@ RESOURCES = {  # noqa: WPS407
 }
 
 
+logger = logging.getLogger(__name__)
+
+
 def get_resources_links(file_path: str, request_url: str) -> List[tuple]:
-    '''
+    """ Get all links of local resources from html file for downloading
 
     :param file_path: path to file for parsing
     :param request_url: url of the web page
     :return: list of tuples with urls for downloading and paths for saving
-    '''
-
+    """
+    logger.debug(f'get_resources_links file_path: {file_path}, request_url: {request_url}')
     soup = BeautifulSoup(read_file(file_path), 'html.parser')
     links = []
-    for tag in soup.find_all(RESOURCES.keys()): # получили теги
-        try:
-            resource_url = tag[RESOURCES[tag.name]]
-        except KeyError:
-            continue
-        # check, that link is local
-        if not is_local(resource_url, request_url):
-            continue
-        # generate path for saving resource
-        local_path = generate_local_path(file_path, resource_url)
-        links.append((make_url_absolute(resource_url, request_url), local_path))
-        tag[RESOURCES[tag.name]] = local_path
+    # get tags with resources
+    for tag in soup.find_all(RESOURCES.keys()):
+        logger.debug(f'processing tag: {tag}')
+        if tag.has_attr(RESOURCES.get(tag.name)):
+            # make absolute url from local url
+            current_url = tag[RESOURCES[tag.name]]
+            resource_url = make_url_absolute(current_url, request_url)
+            logger.debug(f'convert url {current_url} to absolute url: {resource_url}')
 
-    print(soup)
+            # check, if resource is local
+            if is_local(resource_url, request_url):
+                logger.debug(f'url {resource_url} is local')
+                # generate path for saving resource
+                local_path = generate_local_path(file_path, resource_url)
+                links.append((resource_url, local_path))
+                logger.debug(f'add links to list: url for download {resource_url}, path for saving {local_path}')
+                # change tag url to local url
+                tag[RESOURCES[tag.name]] = local_path
+
     write_file(file_path, soup)
-    print(links)
+    logger.debug(f'return {len(links)} links: {str(links)[:200]} ...')
     return links
 
 
-def make_url_absolute(url, request_url):
+def make_url_absolute(url: str, request_url: str) -> str:
+    """Make url absolute
+    :param url: url to make absolute
+    :param request_url: request url, contains hostname
+    :return: absolute url
+    """
+    # fix url if it is relative
+    if url.startswith('//'):
+        url = 'http:' + url
+
     if bool(urlparse(url).netloc):
         return url
     else:
         return urljoin(request_url, url)
 
 
-def read_file(path):
-    with open(path, encoding="utf8") as f:
+def read_file(path: str) -> str:
+    """Read file and return content"""
+    with open(path, encoding="utf-8") as f:
         return f.read()
 
 
-def write_file(path, soup):
-    with open(path, 'w', encoding="utf8") as f:
-        f.write(soup.prettify())
+def write_file(file_path: str, soup: BeautifulSoup):
+    """Write file with parsed html (modified links to local resources)
+
+    :param file_path: path to file for saving
+    :param soup: modified page source
+    """
+    with open(file_path, 'w') as f:
+        f.write(str(soup.prettify()))
 
 
 def is_local(resource_url: str, request_url: str) -> bool:
-    """ Check, is the received path local for host
+    """Check, is the received path local for host
 
     :param resource_url: url of checked resource
     :param request_url: request url, contains hostname
@@ -85,7 +111,6 @@ def is_local(resource_url: str, request_url: str) -> bool:
 
 def generate_local_path(file_path: str, resource_url: str):
     """ Generate path for saving resource
-
     :param file_path: path to saved html file
     :param resource_url: parsed url of resource
     :return: path for saving resource
@@ -97,29 +122,28 @@ def generate_local_path(file_path: str, resource_url: str):
     # make dir, existed dirs allowed
     os.makedirs(folder_path, exist_ok=True)
     # get right filename for saving resource: unique and not too long
-    resource_name = get_resource_name(folder_path, resource_url)
+    resource_name = get_filename(folder_path, resource_url)
     return os.path.join(folder_path, resource_name)
 
 
-def get_resource_name(folder_path: str, resource_url: str) -> str:
-    """
-    Get name for resource from link:
-    - cut name max 20 symbols
+def get_filename(folder_path: str, resource_url: str, original=False) -> str:
+    """Get name for resource from link:
+    - cut name for max 30 symbols
     - if filename is not unique for folder, add random token
-    :param folder_path:
-    :param resource_url:
+    :param folder_path: path to folder for saving resource
+    :param resource_url: url of resource
+    :param original: if True, return filename with added extension '_original'
     :return:
     """
     filename = os.path.basename(urlparse(resource_url).path)    # get filename from url
     name, ext = os.path.splitext(filename)
     if len(filename) > 30:                                      # cut long filenames
-        filename = f'{name[:30]}{ext}'
+        name = f'{name[:30]}'
 
-    if filename in os.listdir(folder_path):                     # generate unique filename if it exist in folder
-        return f'{name[:30]}_{token_urlsafe(8)}{ext}'
+    if f'{name}{ext}' in os.listdir(folder_path):               # generate unique filename if it exist in folder
+        name = f'{name[:30]}_{token_urlsafe(8)}'
 
-    return filename
-
-
-
-
+    if original:
+        return f'{name}_original{ext}'
+    else:
+        return f'{name}{ext}'
