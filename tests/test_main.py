0d2de865daddb21c7680d79c756b99bf17810b82
Ivan Aleksashin <algos.inc@gmail.com>
2022-04-19 00:37:07 +0200

    edit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,6 +10,12 @@ from page_loader.scripts.main import main
 TEST_URL = 'https://habr.ru'
 SAVED_FILE = r'page_loader\scripts\tmp2\habr-ru.html'
 
+# HoWto test
+# - send args
+# - check if file was created
+# - check if resources were downloaded
+# - check if
+
 @pytest.mark.parametrize('test_url', 'saved_file_path'(
     ['-o', '/tmp/', 'https://example.com'],
 def test_main(monkeypatch):
