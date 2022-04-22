533a615af389bfd786ebb5817e07632c1be2ea97
Ivan Aleksashin <algos.inc@gmail.com>
2022-04-22 23:49:21 +0200

    edit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,7 @@ from page_loader.scripts.main import main
 DIR_TO_SAVE = 'tests/tmp_downloads'
 TEST_URL = 'https://habr.ru'
 SAVED_FILE = r'page_loader\scripts\tmp2\habr-ru.html'
-EXPECTED_OUTPUT = ''
+EXPECTED_OUTPUT = 'tests/fixtures/expected_cli_output.txt'
 
 # Howto test
 # - create fixture that create tmpdir
@@ -21,14 +21,17 @@ EXPECTED_OUTPUT = ''
 
 
 @pytest.mark.parametrize(
-    'dir_to_save',
-    'test_url',
-    'saved_file_path',
-    'expected_output',
+    'dir_to_save, test_url, saved_file, expected_output',
     [DIR_TO_SAVE, TEST_URL, SAVED_FILE, EXPECTED_OUTPUT],
 )
-def test_main(monkeypatch):
-    """Test main."""
+def test_main_cli(capsys, monkeypatch, dir_to_save, test_url, saved_file, expected_output):
+    """Test main. Send args and check CLI output. Check if file was created and content is correct."""
     with monkeypatch.context() as m:
-        m.setattr(sys, 'argv', ['page-loader', '-o', 'tmp2', TEST_URL])
-        assert os.path.exists(SAVED_FILE)
+        m.setattr(sys, 'argv', ['page-loader', '-o', dir_to_save, test_url])
+        assert os.path.exists(saved_file)
+
+        captured = capsys.readouterr()
+        with open(expected_output, 'r') as expected_cli_output:
+            assert captured.out == expected_cli_output.read()
+        assert captured.out == expected_output
+
